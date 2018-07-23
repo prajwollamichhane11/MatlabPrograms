bf3100ad945894ea8c346b5d1099683b5d309add
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2018-07-24 00:28:56 +0545

    Create falsepositionmethod.m

---
+++ falsepositionmethod.m
@@ -0,0 +1,25 @@
+a= input('first');
+b = input('last');
+n = input('No. of decimal places:');
+
+tol = (1/2) * 10^(-n);
+
+error  = 1;
+
+if f(a)*f(b) < 0
+  while error > tol 
+    c = (a*f(b) - b*f(a))/(f(b) - f(a)) ;
+    
+    if f(c)*f(a) < 0
+      b = c;
+    else
+      a = c;
+    end
+     error=abs(f(c));
+
+  end
+else
+  disp('Enter the new value range')
+end
+fprintf('%0.4f\n',c)
+
